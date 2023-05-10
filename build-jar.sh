5a79162e71a98e7f606987e459bbc80435a419cb
sreckoj <srecko.janjic@gmail.com>
2023-05-10 09:38:30 +0200

    Delete build-jar.sh

--- build-jar.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This command creates an executable jar with embedded Liberty.
-
-mvn package liberty:create liberty:install-feature liberty:deploy liberty:package -Dinclude=minify,runnable
